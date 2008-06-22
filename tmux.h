5b72c5b01fa0d5a0cd2e587136e213ead2d4f76f
nicm <nicm>
2008-06-22 22:20:07 +0000

    timespecadd, fgetln headers.

--- tmux.h
+++ tmux.h
@@ -75,6 +75,18 @@ extern const char    *__progname;
             ((tsp)->tv_sec cmp (usp)->tv_sec))
 #endif
 
+#ifndef timespecadd
+#define	timespecadd(tsp, usp, vsp)					\
+	do {								\
+		(vsp)->tv_sec = (tsp)->tv_sec + (usp)->tv_sec;		\
+		(vsp)->tv_nsec = (tsp)->tv_nsec + (usp)->tv_nsec;	\
+		if ((vsp)->tv_nsec >= 1000000000L) {			\
+			(vsp)->tv_sec++;				\
+			(vsp)->tv_nsec -= 1000000000L;			\
+		}							\
+	} while (0)
+#endif
+
 #ifndef TTY_NAME_MAX
 #define TTY_NAME_MAX 32
 #endif
