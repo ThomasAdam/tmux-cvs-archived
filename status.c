5c1f8989d37f05f9279e8722967935ae1a440b8f
nicm <nicm>
2008-06-23 21:54:48 +0000

    IRIX fixes, sort of partly work.

--- status.c
+++ status.c
@@ -255,7 +255,18 @@ off:
 size_t
 status_width(struct winlink *wl)
 {
+#ifndef BROKEN_VSNPRINTF
 	return (xsnprintf(NULL, 0, "%d:%s ", wl->idx, wl->window->name));
+#else
+	char	*s; 
+	size_t	n; 
+	
+	xasprintf(&s, "%d:%s ", wl->idx, wl->window->name); 
+	n = strlen(s); 
+	xfree(s); 
+
+	return (n); 
+#endif
 }
 
 char *
