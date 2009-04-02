cb862713377a23d037a2dd6c27b87364ecd034ce
nicm <nicm>
2009-04-02 23:38:37 +0000

    09.

--- cmd-swap-pane.c
+++ cmd-swap-pane.c
@@ -1,7 +1,7 @@
 /* $Id$ */
 
 /*
- * Copyright (c) 2007 Nicholas Marriott <nicm@users.sourceforge.net>
+ * Copyright (c) 2009 Nicholas Marriott <nicm@users.sourceforge.net>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
