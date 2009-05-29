08c41218969472b05a11b777942456f40498c0a3
nicm <nicm>
2009-05-29 05:40:56 +0000

    Don't show real character under cursor when hidden. Doh.

--- status.c
+++ status.c
@@ -658,8 +658,12 @@ status_prompt_redraw(struct client *c)
 	screen_write_cursormove(&ctx, len + c->prompt_index - offset, 0);
 	if (c->prompt_index == strlen(c->prompt_buffer))
 		ch = ' ';
-	else
-		ch = c->prompt_buffer[c->prompt_index];
+	else {
+		if (c->prompt_flags & PROMPT_HIDDEN)
+			ch = '*';
+		else
+			ch = c->prompt_buffer[c->prompt_index];
+	}
 	if (ch == '\0')
 		ch = ' ';
 	gc.attr ^= GRID_ATTR_REVERSE;
