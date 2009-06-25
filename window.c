3ab69f2c85b8e9f85d01c5fb3ca613e3be9ef51d
nicm <nicm>
2009-06-25 15:58:58 +0000

    strdup the input to putenv to avoid in one case passing a string that is later freed and in the other const strings.

--- window.c
+++ window.c
@@ -1,4 +1,4 @@
-/* $Id$ */
+/* $OpenBSD: window.c,v 1.2 2009/06/05 07:18:37 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -487,7 +487,7 @@ window_pane_spawn(struct window_pane *wp,
 		if (chdir(wp->cwd) != 0)
 			chdir("/");
 		for (envq = envp; *envq != NULL; envq++) {
-			if (putenv((char *) *envq) != 0)
+			if (putenv(xstrdup(*envq)) != 0)
 				fatal("putenv failed");
 		}
 		sigreset();
