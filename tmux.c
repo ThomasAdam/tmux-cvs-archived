f7ced722153b4ca5f39228cf952df827a99f1c29
tcunha <tcunha>
2009-08-09 15:26:24 +0000

    Sync OpenBSD patchset 211:
    Add a terminal-overrides session option allowing individual terminfo(5) entries
    to be overridden. The 88col/256col checks are now moved into the default
    setting and out of the code.
    
    Also remove a couple of old workarounds for xterm and rxvt which are no longer
    necessary (tmux can emulate them if missing).

--- tmux.c
+++ tmux.c
@@ -1,4 +1,4 @@
-/* $Id$ */
+/* $Id: tmux.c,v 1.154 2009/08/09 15:26:24 tcunha Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -376,6 +376,8 @@ main(int argc, char **argv)
 		options_set_number(&global_s_options, "status-utf8", 1);
 	else
 		options_set_number(&global_s_options, "status-utf8", 0);
+	options_set_string(&global_s_options,
+	    "terminal-overrides", "*88col*:colors=88,*256col*:colors=256");
 	options_set_number(&global_s_options, "visual-activity", 0);
 	options_set_number(&global_s_options, "visual-bell", 0);
 	options_set_number(&global_s_options, "visual-content", 0);
