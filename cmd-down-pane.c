7951cec793f5bc0d99e2265bc663759405566f23
tcunha <tcunha>
2009-11-14 17:56:39 +0000

    Sync OpenBSD patchset 539:
    Get rid of the ugly CMD_CHFLAG macro and use a const string (eg "dDU") in the
    command entry structs and a couple of functions to check/set the flags.

--- cmd-down-pane.c
+++ cmd-down-pane.c
@@ -1,4 +1,4 @@
-/* $Id$ */
+/* $Id: cmd-down-pane.c,v 1.13 2009/11/14 17:56:39 tcunha Exp $ */
 
 /*
  * Copyright (c) 2009 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -29,7 +29,7 @@ int	cmd_down_pane_exec(struct cmd *, struct cmd_ctx *);
 const struct cmd_entry cmd_down_pane_entry = {
 	"down-pane", "downp",
 	CMD_TARGET_WINDOW_USAGE,
-	0, 0,
+	0, "",
 	cmd_target_init,
 	cmd_target_parse,
 	cmd_down_pane_exec,
--- cmd-select-window.c
+++ cmd-select-window.c
@@ -1,4 +1,4 @@
-/* $Id$ */
+/* $Id: cmd-select-window.c,v 1.24 2009/11/14 17:56:39 tcunha Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -32,7 +32,7 @@ int	cmd_select_window_exec(struct cmd *, struct cmd_ctx *);
 const struct cmd_entry cmd_select_window_entry = {
 	"select-window", "selectw",
 	CMD_TARGET_WINDOW_USAGE,
-	0, 0,
+	0, "",
 	cmd_select_window_init,
 	cmd_target_parse,
 	cmd_select_window_exec,
